9fd52de83f531e0f26af9118fc6c0f865f220985
snsinfu <snsinfu@gmail.com>
2021-01-16 16:59:06 +0900

    examples: Add git pre-receive hook example

---
+++ examples/githook-autodeploy.bash
@@ -0,0 +1,34 @@
+#!/usr/bin/bash -eu
+
+set -o pipefail
+
+while read oldrev newref ref; do
+    branch="$(git rev-parse --symbolic --abbrev-ref "${ref}")"
+
+    case "${branch}" in
+    master | main)
+        ;;
+    *)
+        continue
+    esac
+
+    echo "* Start deploying ${branch}:${newrev}"
+    (
+        workdir="$(mktemp -d)"
+
+        cleanup() {
+            echo "* Cleaning up build..."
+            rm -rf "${workdir}"
+        }
+        trap cleanup EXIT
+
+        echo "* Checking out..."
+        git archive "${newrev}" | tar -C "${workdir}" -xf -
+
+        echo "* Working in ${workdir}"
+        cd "${workdir}"
+
+        imagedeploy
+    )
+    echo "* Finished!"
+done
